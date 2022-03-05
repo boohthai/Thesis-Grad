0642c3f60a491a2b253983db6ee1f18d8fb8b7d3
Thai Thanh Tai <thaithanhtai999@gmail.com>
2022-03-05 20:18:33 +0700

    udpate part 2

--- lib/ResultScreen.dart
+++ lib/ResultScreen.dart
@@ -10,7 +10,7 @@ class ResultPage extends StatefulWidget {
 class _ResultPageState extends State<ResultPage> {
   @override
   Widget build(BuildContext context) {
-    jjuuuuasd this is not a good code
+    jjuuuuasd this is a good code
     _ResultPageState createState() => _ResultPageState();
   }
 }
